37ab8fc55c2eb6125cd79a6c7649a73e9192ac05
Davi <123982670+davihoehene@users.noreply.github.com>
2024-09-07 18:40:30 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "BK",
+        descricao: "Uma das vozes mais contundentes dentro do rap nacional, Abebe Bikila, mais conhecido como BK', é um rapper carioca que carrega em suas rimas as vivências de um corpo negro dentro das grandes metrópoles. Seus versos, além de críticas sociais e conquistas, também narram um outro lado do rolê, trazendo as situações mais nuas e cruas possíveis. Em 2021, o cantor fundou o seu próprio selo, Gigantes, por onde lançou o EP Cidade do Pecado. O nome dado ao selo faz referência ao seu segundo trabalho de estúdio, Gigantes (2018), que sucedeu o debut Castelos & Ruínas (2016) e antecedeu O Líder Em Movimento (2020). Em 2022, BK' chega com o seu quarto álbum de estúdio, ICARUS, uma perspectiva contemporânea e urbana do mito de Ícaro.",
+        link: "https://open.spotify.com/intl-pt/artist/1YOVBTvznjiDvtAj4ExHeo?si=a2PvRlqHSIyk3RYHpUwIYQ",
+        tags: "melhor"
+    },
+    {
+        titulo: "Filipe Ret",
+        descricao: "Filipe Ret é um rapper, cantor e compositor carioca, nascido e criado no Rio de Janeiro. Com mais de 15 anos de carreira e acumulando bilhões de streams, Ret recebeu o prêmio de melhor álbum do ano de 2022 com LUME no Prêmio Multishow e foi indicado ao Grammy Latino na categoria Melhor Interpretação Urbana em Língua Portuguesa com Good Vibe. Carregando a bandeira do Rap não só como artista, Ret é empresário e dono do selo Nadamal, um dos principais da cena do rap hoje.",
+        link: "https://open.spotify.com/intl-pt/artist/7gJN8W0589FisSYJS17K54?si=ra7RyKuIRxWIAmPZGS2HrA",
+        tags: "catete"
+    },
+    {
+        titulo: "Djonga",
+        descricao: "O rapper brasileiro Djonga combina rimas agressivas e com forte carga social com melodias suaves, inspiradas no samba e funk, gêneros musicais com os quais cresceu. Nascido Gustavo Pereira Marques em 1994 na cidade de Belo Horizonte, no sudeste do Brasil, filho de um servidor...",
+        link: "https://open.spotify.com/intl-pt/artist/204IwDdaHE4ymGk9Kya2pY?si=kRTxPAcIRsCoXbxOALmPGg",
+        tags: "fogo nos racistas"
+    }
+];
